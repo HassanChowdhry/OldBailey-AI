ade0095bb8b235b4be535cc41700a47fb152a67c
Hassan Chowdhry <94680098+HassanChowdhry@users.noreply.github.com>
2024-07-24 12:38:00 -0700

    server updates

--- server/src/modules/__init__.py
+++ server/src/modules/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
-from .controller import threads
+from .controller import threads, auth
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def create_app():
   app = Flask(__name__)
   CORS(app)
+  
+  app.config["MONGO_URI"] = os.getenv("MONGO_URI")
   app.register_blueprint(threads, url_prefix="/api")
+  app.register_blueprint(auth, url_prefix="/auth")
         
   return app
