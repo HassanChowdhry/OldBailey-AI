ed11b9b8cfdbed0781bd4597f0bf1c4a49d19527
Hassan Chowdhry <94680098+HassanChowdhry@users.noreply.github.com>
2024-08-13 01:37:17 -0700

    ...

--- app/components/ChatMessage.tsx
+++ app/components/ChatMessage.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import Markdown from 'react-markdown'
-import { FaBold } from "react-icons/fa";
+import { RiBardLine } from "react-icons/ri";
+import { LuCopy } from "react-icons/lu";
+import { MdDone } from "react-icons/md";
 import remarkGfm from "remark-gfm";
 import rehypeHighlight from 'rehype-highlight';
 import PFP from '@/components/Pfp';
+import Tip from '@/components/Tip';
 
 import "./ChatMessage.scss";
 import 'highlight.js/styles/obsidian.css';  
+import { cn } from '@/lib/utils';
+import { useState } from 'react';
 
 type ChatMessageProps = {
   message: string;
@@ -13,22 +20,44 @@ type ChatMessageProps = {
 }
 
 export default function ChatMessage({ message, role }: ChatMessageProps) {
+  const [isCopied, setIsCopied] = useState(false);
   const isUser = role === "user";
   const userStyles = "bg-maroon-2 max-w-[70%] w-fit h-full rounded-3xl px-5 py-2.5"
   
   return (
-    <div className='flex mx-2 my-4 p-2 text-white-1/85'>
-        <div className="rounded-full h-8 min-w-8 bg-tranparent border border-white-2/30 shadow-inner shadow-white-2/20 text-white-1 flex items-center justify-center">
-          {isUser ? (<PFP disabled={true}/>) : (<FaBold />)}
+    <div className='flex mx-2 my-4 p-2 gap-6 text-white-2'>
+        <div className="rounded-full h-8 min-w-8 mt-1 bg-tranparent border border-white-2/30 shadow-inner shadow-white-2/20 text-white-1 flex items-center justify-center">
+          {isUser ? (<PFP disabled={true}/>) : (<RiBardLine />)}
         </div>
-        <div className={`${isUser ? userStyles: "px-2.5"} flex flex-col h-full my-auto ml-3  max-w-full overflow-x-scroll`}>
+        <div className={`${isUser ? userStyles: "px-2.5"} text-white-1/90 gap-1 flex flex-col h-full my-auto ml-1 max-w-full overflow-x-scroll`}>
             <Markdown
-                className="text-white-100 markdown"
+                className={cn("text-white-100 markdown", !isUser && "p-1")}
                 remarkPlugins={[remarkGfm]}
                 rehypePlugins={[rehypeHighlight]}
             >
               {message}
             </Markdown>
+
+            {!isUser && (
+                <Tip 
+                  TipContent="Copy"
+                  TipSide="left"
+                  TipTrigger={
+
+                    <button 
+                      onClick={() => {
+                        setIsCopied(true)
+                        navigator.clipboard.writeText(message)
+                        setTimeout(() => {
+                          setIsCopied(false);
+                        }, 5000);
+                      }}
+                      className={cn('p-2 ml-[-2px] cursor-pointer rounded-lg w-max hover:bg-maroon-2 duration-300 hover:text-white-1', isCopied && "bg-maroon-2 text-white-1")}>
+                      {!isCopied ? <LuCopy /> : <MdDone />}
+                    </button>
+                  }
+                />
+            )}
         </div>
     </div>
   )
--- server/src/modules/services/otp.py
+++ server/src/modules/services/otp.py
@@ -1,4 +1,5 @@
-import os, random, json
+import random, json, boto3
+from botocore.exceptions import ClientError
 from modules.db import redis_client
 from dotenv import load_dotenv
 
@@ -51,5 +52,63 @@ def update_otp(email, new_otp):
         return True
     return False
 
+# TODO: Update this
 def send_otp_email(email, otp):
-  print(f"Sending OTP {otp} to {email}")
+    SENDER = "mohammedchowdhry11@gmail.com"
+
+    RECIPIENT = email
+
+    SUBJECT = "Amazon SES Test (SDK for Python)"
+
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = ("Amazon SES Test (Python)\r\n"
+                f"Your OTP is: {otp} \r\n"
+                "This email was sent with Amazon SES using the "
+                "AWS SDK for Python (Boto)."
+                )
+                
+    BODY_HTML = f"""<html>
+    <head></head>
+    <body>
+    <h1>Amazon SES Test (SDK for Python)</h1>
+    <p>This email was sent with
+        <strong>Your OTP is: <em>{otp}</em> \r\n</strong>
+        <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+        <a href='https://aws.amazon.com/sdk-for-python/'>
+        AWS SDK for Python (Boto)</a>.</p>
+    </body>
+    </html>
+                """            
+    CHARSET = "UTF-8"
+
+    client = boto3.client('ses')
+
+    try:
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print(f'OTP {otp} sent to {email}')
